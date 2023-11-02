849819f4d70d51ed553281f495a77c77469787ef
vivek-78 <akepoguvivek14@gmail.com>
2023-11-03 01:01:15 +0530

    created issue table

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,18 +7,32 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/createIssueSchema";
 import { z } from "zod";
 import { useRouter } from "next/navigation";
+import Spinner from "@/app/Spinner";
 
 type issueForm = z.infer<typeof createIssueSchema>;
 function NewIssue() {
+  const [error, setError] = useState(false);
+  const [spinner, setSpinner] = useState(false);
   const router = useRouter();
   const {
     register,
     handleSubmit,
-    formState : { errors },
+    formState: { errors },
   } = useForm<issueForm>({
     resolver: zodResolver(createIssueSchema),
   });
-  const [error, setError] = useState(false);
+  const onSubmit = () => {
+    handleSubmit(async (data) => {
+      try {
+        setSpinner(true);
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      } catch (err) {
+        setSpinner(false);
+        setError(true);
+      }
+    });
+  };
   return (
     <div className="max-w-xl">
       {error && (
@@ -26,32 +40,31 @@ function NewIssue() {
           <Callout.Text>Unexpected error has occured</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="max-w-xl"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await axios.post("/api/issues",data);
-            router.push("/issues")
-          } catch (err) {
-            console.log(err);
-            setError(true);
-          }
-        })}
-      >
-        {errors.title && <Text color="red" as="p">Title is required</Text>}
+      <form className="max-w-xl" onSubmit={onSubmit}>
+        {errors.title && (
+          <Text color="red" as="p">
+            Title is required
+          </Text>
+        )}
         <TextField.Root className="max-w-xl mb-2">
           <TextField.Input
             placeholder="Enter Issue Title"
             {...register("title")}
           />
         </TextField.Root>
-        {errors.description && <Text color="red" as="p">Description is required</Text>}
+        {errors.description && (
+          <Text color="red" as="p">
+            Description is required
+          </Text>
+        )}
         <TextArea
           className="max-w-xl mb-3"
           placeholder="Enter Issue Description"
           {...register("description")}
         />
-        <Button>Create New Issue</Button>
+        <Button disabled={spinner}>
+          Create New Issue {spinner && <Spinner />}
+        </Button>
       </form>
     </div>
   );
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,42 @@
-"use client"
-import React from 'react'
-import {Button} from "@radix-ui/themes";
-import {useRouter} from "next/navigation";
+import React from "react";
+import { Button, Table } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
 
-function Issues() {
-  const router = useRouter();
+// eslint-disable-next-line @next/next/no-async-client-component
+const Issues = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-     <Button onClick={()=>router.push("/issues/new")}>New Issues</Button>
+      <Button>New Issues</Button>
+      <Table.Root variant="surface" className="mt-2">
+        <Table.Header>
+          <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className="hidden md:table-cell">
+            Status
+          </Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell className="hidden md:table-cell">
+            Created Date
+          </Table.ColumnHeaderCell>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block mb:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export default Issues
+export default Issues;
